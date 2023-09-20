d6c2385fc03a34c4f1a12288e2e6f14f39b06a1b
elite0107 <toptalent0107@gmail.com>
2023-09-20 18:00:02 +0200

    Add Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="flex items-center justify-center w-screen h-screen text-3xl font-bold underline text-red-600">
-          Simple React Typescript Tailwind Sample
-        </h1>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="flex items-center justify-center w-screen h-screen text-3xl font-bold underline text-red-600">
+            Simple React Typescript Tailwind Sample
+          </h1>
+        </header>
+      </div>
+    </Provider>
   );
 }
 
